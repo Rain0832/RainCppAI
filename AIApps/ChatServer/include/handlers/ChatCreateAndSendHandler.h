f5bb70b5a248a908c0f91e9bf410c81a77bc4b86
raincbhuang <raincbhuang@tencent.com>
2026-02-11 19:13:21 +0800

    style: Improve formatting and comments for clarity in ChatCreateAndSendHandler class

--- AIApps/ChatServer/include/handlers/ChatCreateAndSendHandler.h
+++ AIApps/ChatServer/include/handlers/ChatCreateAndSendHandler.h
@@ -2,20 +2,26 @@
 #include "../../../../HttpServer/include/router/RouterHandler.h"
 #include "../../../HttpServer/include/utils/MysqlUtil.h"
 
-#include"../AIUtil/AISessionIdGenerator.h"
+#include "../AIUtil/AISessionIdGenerator.h"
 #include "../ChatServer.h"
-
-
 
 class ChatCreateAndSendHandler : public http::router::RouterHandler
 {
 public:
-    explicit ChatCreateAndSendHandler(ChatServer* server) : server_(server) {}
+    // 构造函数。
+    //
+    // Args:
+    //   server: 业务服务器指针。
+    explicit ChatCreateAndSendHandler(ChatServer *server) : server_(server) {}
 
-    void handle(const http::HttpRequest& req, http::HttpResponse* resp) override;
-private:
+    // 创建新会话并处理聊天请求，返回 AI 响应结果。
+    //
+    // Args:
+    //   req: HTTP 请求对象。
+    //   resp: HTTP 响应对象。
+    void handle(const http::HttpRequest &req, http::HttpResponse *resp) override;
 
 private:
-    ChatServer* server_;
-    http::MysqlUtil     mysqlUtil_;
+    ChatServer *server_;
+    http::MysqlUtil mysqlUtil_;
 };
